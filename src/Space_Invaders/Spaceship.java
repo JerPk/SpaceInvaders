f720c09af9c0a8f68d7edd49512a144c421c4a6f
Jeroen <Jeroen>
2015-09-07 13:09:51 +0200

    Added X/Y position and getPosX/getPosY for Spaceship and Alien.

--- src/Space_Invaders/Spaceship.java
+++ src/Space_Invaders/Spaceship.java
@@ -1,5 +1,25 @@
 package Space_Invaders;
 
 public class Spaceship {
-
+	
+	// the x and y coordinates of the Spaceship.
+	private int x, y;
+	
+	/**
+	 * the method that returns the x position
+	 * 
+	 * @return x position
+	 */
+	public int getPosX(){
+		return x;
+	}
+	
+	/**
+	 * the method that returns the y position
+	 * 
+	 * @return y position
+	 */
+	public int getPosY(){
+		return y;
+	}
 }
