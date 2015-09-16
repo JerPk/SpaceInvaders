1693372d511cd23ecf8fbc5e4bfecc3f1bfd03f9
harveyvanveltom <harvey-shadow@hotmail.com>
2015-09-17 00:12:05 +0200

    Finished Testing + static analasis of test methods
    finished the Game test. the overall coverage now is 77%. Also used PMD,
    checkstyle and findbugs to remove as many bugs and stylistic errors as
    possible.

--- src/Space_Invaders_Test/BarrierTest.java
+++ src/Space_Invaders_Test/BarrierTest.java
@@ -2,96 +2,105 @@ package Space_Invaders_Test;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Vector;
+import Space_Invaders.Barrier;
+import Space_Invaders.Bullet;
+import Space_Invaders.Game;
+import Space_Invaders.SpriteSheet;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import Space_Invaders.Barrier;
-import Space_Invaders.Bullet;
-import Space_Invaders.Game;
-import Space_Invaders.Spaceship;
-import Space_Invaders.SpriteSheet;
-
+import java.util.Vector;
+/**
+ * BarrierTest tests all the methods in the Barrier class.
+ * 
+ * @author Group 23
+ *
+ */
 public class BarrierTest {
-    private Game g;
-
+    
     /**
-     * Launch the user interface.
-     */
-    @Before
+     * The game object that is used in all of the test cases.
+     */ 
+  private Game game;
+
+  /**
+   * This method is executed before every test.
+   * It creates the game class and sets up the required spritesheet.
+   */
+  @Before
     public void setUpGame() {
-        g = new Game();
-        g.setSpriteSheet("res/sprite_sheet.png");
-    }
+    game = new Game();
+    game.setSpriteSheet("res/sprite_sheet.png");
+  }
 
     /**
      * the JUnit test of the Constuctor method of Barrier.
      */
-    @Test
+  @Test
     public void testBarrier() {
-        Barrier b = new Barrier(3.0, 3.0, new SpriteSheet(g.getSpriteSheet()));
+    final Barrier barrier = new Barrier(3.0, 3.0, new SpriteSheet(game.getSpriteSheet()));
 
-        assertEquals(b.getPosX(), 3.0, 0.00001);
-        assertEquals(b.getPosY(), 3.0, 0.00001);
-        assertEquals(b.getState(), 0);
+    assertEquals(barrier.getPosX(), 3.0, 0.00001);
+    assertEquals(barrier.getPosY(), 3.0, 0.00001);
+    assertEquals(barrier.getState(), 0);
 
-    }
+  }
 
     /**
      * The JUnit test of the IfHit method of Barrier.
      */
-    @Test
+  @Test
     public void testIfHit() {
-        Barrier b = new Barrier(3.0, 3.0, new SpriteSheet(g.getSpriteSheet()));
-        Vector<Bullet> alienBullets = new Vector<Bullet>(0);
+    final Barrier barrier = new Barrier(3.0, 3.0, new SpriteSheet(game.getSpriteSheet()));
+    final Vector<Bullet> alienBullets = new Vector<Bullet>(0);
 
-        assertEquals(b.ifHit(alienBullets), -1);
+    assertEquals(barrier.ifHit(alienBullets), -1);
 
-        SpriteSheet ss = new SpriteSheet(g.getSpriteSheet());
+    final SpriteSheet spritesheet = new SpriteSheet(game.getSpriteSheet());
 
-        Bullet testBullet = new Bullet(20, 15, ss);
-        alienBullets.add(testBullet);
+    final Bullet testBullet = new Bullet(20, 15, spritesheet);
+    alienBullets.add(testBullet);
         
-        assertEquals(b.ifHit(alienBullets),0);
+    assertEquals(barrier.ifHit(alienBullets),0);
 
-    }
+  }
     
     /**
      * The JUnit test of the decreaseState method of Barrier.
      */
-    @Test
-    public void testDecreaseState(){
-        Barrier b = new Barrier(3.0, 3.0, new SpriteSheet(g.getSpriteSheet()));
-        assertEquals(b.getState(),0);
-        b.decreaseState();
-        assertEquals(b.getState(),1);
-    }
+  @Test
+    public void testDecreaseState() {
+    final Barrier barrier = new Barrier(3.0, 3.0, new SpriteSheet(game.getSpriteSheet()));
+    assertEquals(barrier.getState(),0);
+    barrier.decreaseState();
+    assertEquals(barrier.getState(),1);
+  }
     
     /**
      * The JUnit test of the getPosX method of Barrier.
      */
-    @Test
-    public void testGetPosX(){
-        Barrier b = new Barrier(3.0, 5.0, new SpriteSheet(g.getSpriteSheet()));
-        assertEquals(b.getPosX(),3.0,0.00001);
-    }
+  @Test
+    public void testGetPosX() {
+    final Barrier barrier = new Barrier(3.0, 5.0, new SpriteSheet(game.getSpriteSheet()));
+    assertEquals(barrier.getPosX(),3.0,0.00001);
+  }
     
     /**
      * The JUnit test of the getPosY method of Barrier.
      */
-    @Test
-    public void testGetPosY(){
-        Barrier b = new Barrier(3.0, 5.0, new SpriteSheet(g.getSpriteSheet()));
-        assertEquals(b.getPosY(),5.0,0.00001);
-    }
+  @Test
+    public void testGetPosY() {
+    final Barrier barrier = new Barrier(3.0, 5.0, new SpriteSheet(game.getSpriteSheet()));
+    assertEquals(barrier.getPosY(),5.0,0.00001);
+  }
     
     /**
      * The JUnit test of the getState method of Barrier.
      */
-    @Test
-    public void testGetState(){
-        Barrier b = new Barrier(3.0, 5.0, new SpriteSheet(g.getSpriteSheet()));
-        assertEquals(b.getState(),0);
-    }
+  @Test
+    public void testGetState() {
+    final Barrier barrier = new Barrier(3.0, 5.0, new SpriteSheet(game.getSpriteSheet()));
+    assertEquals(barrier.getState(),0);
+  }
 }
--- src/Space_Invaders_Test/SpaceshipTest.java
+++ src/Space_Invaders_Test/SpaceshipTest.java
@@ -1,118 +1,127 @@
 package Space_Invaders_Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.util.Vector;
+import Space_Invaders.Bullet;
+import Space_Invaders.Game;
+import Space_Invaders.Spaceship;
+import Space_Invaders.SpriteSheet;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import Space_Invaders.Spaceship;
-import Space_Invaders.Bullet;
-import Space_Invaders.Game;
-import Space_Invaders.SpriteSheet;
+import java.util.Vector;
 
+/**
+ * SpaceshipTest tests all the methods in the spaceship class.
+ * 
+ * @author Group 23
+ */
 public class SpaceshipTest {
-	
-	private Game g;
-	
-	/**
-	 * Launch the user interface.
-	 */
-	@Before
-	public void setUpGame() {
-		g = new Game();
-		g.setSpriteSheet("res/sprite_sheet.png");
-	}
-	
-	/**
-	 * the JUnit test of the Constuctor method of Spaceship.
-	 */
-	@Test
-	public void testSpaceship() {
-		Spaceship s = new Spaceship(g);
-		assertEquals((double) 304, s.getPosX(), 0.00001);
-		assertEquals((double) 425, s.getPosY(), 0.00001);
-	}
-	
-	/**
-	 * the JUnit test of the MoveLeft method of Spaceship.
-	 */
-	@Test
-	public void testMoveLeft() {
-		Spaceship s = new Spaceship(g);
-        g.setrunning(true);
-		
-		s.moveLeft();
-		assertEquals((double) 302, s.getPosX(), 0.00001);
-		assertEquals(true, g.getrunning());
-	}
+  
+    /**
+     * The game object that is used in all of the test cases.
+     */
+  private Game game;
+  
+  /**
+   * Launch the user interface.
+   */
+  @Before
+  public void setUpGame() {
+    game = new Game();
+    game.setSpriteSheet("res/sprite_sheet.png");
+  }
+  
+  /**
+   * the JUnit test of the Constuctor method of Spaceship.
+   */
+  @Test
+  public void testSpaceship() {
+    final Spaceship ship = new Spaceship(game);
+    assertEquals((double) 304, ship.getPosX(), 0.00001);
+    assertEquals((double) 425, ship.getPosY(), 0.00001);
+  }
+  
+  /**
+   * the JUnit test of the MoveLeft method of Spaceship.
+   */
+  @Test
+  public void testMoveLeft() {
+    final Spaceship ship = new Spaceship(game);
+    game.setrunning(true);
+    
+    ship.moveLeft();
+    assertEquals((double) 302, ship.getPosX(), 0.00001);
+    assertTrue(game.getrunning());
+  }
 
-	/**
-	 * the JUnit test of the MoveRight method of Spaceship.
-	 */
-	@Test
-	public void testMoveRight() {
-		Spaceship s = new Spaceship(g);
-        g.setrunning(true);
-		
-		s.moveRight();
-		assertEquals((double) 306, s.getPosX(), 0.00001);
-		assertEquals(true, g.getrunning());
-	}
+  /**
+   * the JUnit test of the MoveRight method of Spaceship.
+   */
+  @Test
+  public void testMoveRight() {
+    final Spaceship ship = new Spaceship(game);
+    game.setrunning(true);
+    
+    ship.moveRight();
+    assertEquals((double) 306, ship.getPosX(), 0.00001);
+    assertTrue(game.getrunning());
+  }
 
-	/**
-	 * the JUnit test of the Shoot method of Spaceship.
-	 */
-	@Test
-	public void testShoot() {
-		Spaceship s = new Spaceship(g);
-        g.setrunning(true);
-		
-		Bullet b = s.shoot();
-		assertEquals((double) s.getPosX() + 10, b.getX(), 0.00001);
-		assertEquals((double) s.getPosY() + 2, b.getY(), 0.00001);
-		assertEquals(true, g.getrunning());
-	}
+  /**
+   * the JUnit test of the Shoot method of Spaceship.
+   */
+  @Test
+  public void testShoot() {
+    final Spaceship ship = new Spaceship(game);
+    game.setrunning(true);
+    
+    final Bullet bullet = ship.shoot();
+    assertEquals((double) ship.getPosX() + 10, bullet.getX(), 0.00001);
+    assertEquals((double) ship.getPosY() + 2, bullet.getY(), 0.00001);
+    assertTrue(game.getrunning());
+  }
 
-	/**
-	 * the JUnit test of the ifHit method of Spaceship.
-	 */
-	@Test
-	public void testIfHit() {
-		Spaceship s = new Spaceship(g);
-        g.setrunning(true);
-        Vector<Bullet> alienBullets = new Vector<Bullet>(0);
+  /**
+   * the JUnit test of the ifHit method of Spaceship.
+   */
+  @Test
+  public void testIfHit() {
+    final Spaceship ship = new Spaceship(game);
+    game.setrunning(true);
+    final Vector<Bullet> alienBullets = new Vector<Bullet>(0);
         
-        alienBullets.add(new Bullet(5, 5, new SpriteSheet(g.getSpriteSheet())));
-       assertEquals(s.ifHit(alienBullets),-1); 
-       alienBullets.removeAllElements();
+    alienBullets.add(new Bullet(5, 5, new SpriteSheet(game.getSpriteSheet())));
+    assertEquals(ship.ifHit(alienBullets),-1); 
+    alienBullets.removeAllElements();
         
-        alienBullets.add(new Bullet(317, 425, new SpriteSheet(g.getSpriteSheet())));
-       assertEquals(s.ifHit(alienBullets),0);
-	}
-	
-	/**
-	 * The JUnit test of the getlives method of Spaceship. 
-	 * the getlives method returns the amount of lives the spaceship has left.
-	 */
-	@Test
-	public void testGetLives(){
-	    Spaceship s = new Spaceship(g);
-	    assertEquals(s.getLives(),3);
-	}
-	
-	/**
+    alienBullets.add(new Bullet(317, 425, new SpriteSheet(game.getSpriteSheet())));
+    assertEquals(ship.ifHit(alienBullets),0);
+  }
+  
+  /**
+   * The JUnit test of the getlives method of Spaceship. 
+   * the getlives method returns the amount of lives the spaceship has left.
+   */
+  @Test
+  public void testGetLives() {
+    final Spaceship ship = new Spaceship(game);
+    assertEquals(ship.getLives(),3);
+  }
+  
+  /**
      * The JUnit test of the decreaseLives method of Spaceship. 
      * every time the decreaseLives method is called the amount of lives spaceship
      * should be decreased by one.
      */
-	@Test
-	public void testDecreaseLives(){
-	    Spaceship s = new Spaceship(g);
-        assertEquals(s.getLives(),3);
-        s.decreaseLives();
-        assertEquals(s.getLives(),2);
-	}
+  @Test
+  public void testDecreaseLives() {
+    final Spaceship ship = new Spaceship(game);
+    assertEquals(ship.getLives(),3);
+    ship.decreaseLives();
+    assertEquals(ship.getLives(),2);
+  }
 
 }
