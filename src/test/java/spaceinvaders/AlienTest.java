a50d1f468c9f9acb0285a29ff6ec4402a203b5ca
Jeroemaster <Jeroemaster@hotmail.com>
2015-10-30 17:09:09 +0100

    <organisation Fix Travis build error

--- src/test/java/spaceinvaders/AlienTest.java
+++ src/test/java/spaceinvaders/AlienTest.java
@@ -1,4 +1,4 @@
-package spaceinvaders.spaceinvaders_framework;
+package spaceinvaders;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
--- src/test/java/spaceinvaders/LevelTest.java
+++ src/test/java/spaceinvaders/LevelTest.java
@@ -1,4 +1,4 @@
-package spaceinvaders.spaceinvaders_framework;
+package spaceinvaders;
 
 //import static org.junit.Assert.assertFalse;
 //import static org.junit.Assert.assertNotSame;
