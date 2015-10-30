9c86ac35c1472aed06205d5e768c01e479658397
Jeroemaster <Jeroemaster@hotmail.com>
2015-10-30 23:36:10 +0100

    <documentation Checked tools in iterator package

--- src/main/java/iterator/ConcreteIterator.java
+++ src/main/java/iterator/ConcreteIterator.java
@@ -2,33 +2,62 @@ package iterator;
 
 import java.util.Vector;
 
+/**
+ * The ConcreteIterator class which implements the iterator.
+ * 
+ * @author Group 23
+ */
 public class ConcreteIterator implements Iterator {
-  Vector<?> items;
 
-  int position = 0;
+  /**
+   * The vector with the items.
+   */
+  private Vector<?> items;
 
-  public ConcreteIterator(Vector<?> newItems) {
+  /**
+   * The position of the iterator.
+   */
+  private int position = 0;
+
+  /**
+   * Constructor of the iterator.
+   * 
+   * @param newItems
+   *          the items to iterate over
+   */
+  public ConcreteIterator(final Vector<?> newItems) {
     this.items = newItems;
   }
 
+  /**
+   * Method to check if there is a next element.
+   * 
+   * @return has a next element or not
+   */
   @Override
-  public boolean hasNext() {
-    if (position >= items.size() || items.get(position) == null) {
-      return false;
-    } else {
-      return true;
-    }
+  public final boolean hasNext() {
+    return !(position >= items.size() || items.get(position) == null);
   }
 
+  /**
+   * Method to get next element.
+   * 
+   * @return the next object
+   */
   @Override
-  public Object next() {
+  public final Object next() {
     Object nextObject = items.get(position);
     position = position + 1;
     return nextObject;
   }
 
+  /**
+   * Method to lower the position with one.
+   * 
+   * @return the new position
+   */
   @Override
-  public int position() {
+  public final int position() {
     return position - 1;
   }
 
--- src/main/java/iterator/Iterator.java
+++ src/main/java/iterator/Iterator.java
@@ -1,11 +1,31 @@
 package iterator;
 
+/**
+ * The Iterator class.
+ * 
+ * @author Group 23
+ */
 public interface Iterator {
 
+  /**
+   * Method to check if there is a next element.
+   * 
+   * @return has a next element or not
+   */
   boolean hasNext();
 
+  /**
+   * Method to get next element.
+   * 
+   * @return the next object
+   */
   Object next();
 
+  /**
+   * Method to lower the position with one.
+   * 
+   * @return the new position
+   */
   int position();
 
 }
